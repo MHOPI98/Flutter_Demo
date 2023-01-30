641d5efcdd5241ca87faa478e092b72df637a959
MHOPI98 <bdmhopi@gmail.com>
2023-01-30 21:00:14 +0600

    Day 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/homepage.dart';
+import 'package:flutter_application_1/screens/login_page.dart';
 
 void main() => runApp(MyApp(
     ));
@@ -9,11 +10,16 @@ class MyApp extends   StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: homepage(),
+      
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
+      routes: {
+        "/": (context)=>login(),
+        "/login":(context)=>homepage(),
+        "/login":(context)=>login(),
+      },
     );
   }
 }
